2cd792bd1e6ca50840a28c2901652a92897f1ec3
Rafa Figueiredo <rafaelfigueiredojunior7@gmail.com>
2021-08-16 20:32:11 -0300

    telaEmpresaCadastro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {React} from "react";
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { CandidatoForm } from "./pages/CandidatoCadastro/Candidato";
+import { EmpresaForm } from "./pages/EmpresaCadastro/EmpresaForm";
 
 
 import {Home} from "./pages/Home/Home";
@@ -14,6 +15,8 @@ function App() {
     <Route path="/" exact component={Home}/>
     <Route path="/login" component={Login}/>
     <Route path="/candidato" component={CandidatoForm}/>
+    <Route path="/empresaform" component={EmpresaForm}/>
+
     </Switch>
     </BrowserRouter>
     
