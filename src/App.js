202dee370ea6981e1a3ae250d3f5708b77e0aad9
isabellasantiago <isabella.-.bela@hotmail.com>
2021-10-01 20:57:43 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import { CandidatoForm } from "./pages/CandidatoCadastro/Candidato";
 import { EmpresaForm } from "./pages/EmpresaCadastro/EmpresaForm";
 import {Home} from "./pages/Home/Home";
 import{Login} from "./pages/Login/Login";
-<<<<<<< HEAD
 import {Curriculo} from './pages/cadastroCurriculo/Curriculo';
-=======
 import { ModalCandidato } from "./pages/ModalCandidato/ModalCandidato";
->>>>>>> dev
+
 
 function App() {
   return (
@@ -19,13 +17,10 @@ function App() {
     <Route path="/" exact component={Home}/>
     <Route path="/login" component={Login}/>
     <Route path="/candidato" component={CandidatoForm}/>
-<<<<<<< HEAD
     <Route path="/modal-candidatos" component={ModalCandidato}/>
-=======
     <Route path="/empresaform" component={EmpresaForm}/>
     <Route path="/cv" component={Curriculo}/>
 
->>>>>>> dev
     </Switch>
     </BrowserRouter>
     
