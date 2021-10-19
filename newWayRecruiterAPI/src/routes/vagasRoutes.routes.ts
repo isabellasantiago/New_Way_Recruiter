fbed95825395f41af3a9f6d9b2f7f36c86181b94
isabellasantiago <isabella.-.bela@hotmail.com>
2021-10-19 09:34:45 -0300

    changes

--- newWayRecruiterAPI/src/routes/vagasRoutes.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import vagasController from "../controller/vagasController";
-
-const vagasRouter = express.Router();
-
-vagasRouter.post("/vaga", vagasController.create);     //cria uma vaga
-vagasRouter.get("/allvagas", vagasController.findAll);    //lista todas as vagas
-
-export { vagasRouter };
